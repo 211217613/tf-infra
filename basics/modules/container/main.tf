39c4fbedcab430cb2e09a2d532e0dce79e3ef11a
Ruben Vazquez <rubenavazquez@gmail.com>
2019-07-30 15:12:20 -0400

    Add container module

---
+++ basics/modules/container/main.tf
@@ -0,0 +1,8 @@
+resource "docker_container" "container_id" {
+  name  = "${var.container_name}"
+  image = "${var.image}"
+  ports {
+    internal = "${var.internal_port}"
+    external = "${var.external_port}"
+  }
+}
---
+++ basics/modules/container/variables.tf
@@ -0,0 +1,11 @@
+########################################
+#   VARIABLES
+########################################
+
+variable "container_name" {}
+variable "image" {}
+variable "internal_port" {}
+variable "external_port" {}
+
+
+
